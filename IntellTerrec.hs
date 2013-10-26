73170e34d0f89bb9aac1c75d64f64a6c41847712
vauhochzett <valentin_zieglmeier@gmx.de>
2013-10-26 12:18:27 +0200

    Redo

--- IntellTerrec.hs
+++ IntellTerrec.hs
@@ -1,24 +1,11 @@
 module IntellTerrec where
-import Data.DateTime
+
 {- Idee: Terminlisten der Tutorgruppen aus TUMOnline herunterladen
  - und einlesen, beste Terminkombination nach folgenden Voraussetz-
  - ungen finden:
  - .. möglichst wenige Tage in der Uni
  - .. möglichst kurze Wartezeiten
  - .. möglichst kurze Laufwege -}
-<<<<<<< HEAD
- 
-
- {- 
- {- IO -}
- 
- -- to implement
- 
- 
- {- Matching -}
- 
- 
-=======
 
 
 {- Konstanten -}
@@ -34,4 +21,3 @@ import Data.DateTime
 {- Matching -}
 
 
->>>>>>> 221ea55b44feba2076712d233034ffcf471bc998
