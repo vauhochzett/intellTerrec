6b7bffd1e201d03ca70cbdbd128ebcaf00df9c4c
asaljic <strikerplayer@35.193.eduroam.dynamic.rbg.tum.de>
2013-10-26 18:01:37 +0200

    hahah

--- IntellTerrec.hs
+++ IntellTerrec.hs
@@ -9,27 +9,6 @@ import Csv9
  - .. möglichst kurze Laufwege -}
 
 
-{- Typen -}
-
-type Day = Integer
-type Hour = Integer
-type Minute = Integer
-type GNumber = String
-
-type GroupList = [Group]
-type DateList = [Date]
-
-
-data Time = Time {hour :: Hour, minute :: Minute}
-	deriving (Show)	-- Von, Bis
-
-data Date = Date {dday :: Day, dstartTime :: Time, dendTime :: Time} 
-	deriving (Show)
-
-data Group = Group {gNumber :: GNumber, gday :: Day, gstartTime :: Time, gendTime :: Time}
-	deriving (Show)
-
-
 {- Konstanten (Testing) -}
 
 freeSlot :: DateList
