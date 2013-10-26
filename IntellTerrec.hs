6d4cb46ce4fb6151dcfae1904acb5722827f22d4
vauhochzett <valentin_zieglmeier@gmx.de>
2013-10-26 11:45:52 +0200

    Idee als Kommentar

--- IntellTerrec.hs
+++ IntellTerrec.hs
@@ -1,2 +1,10 @@
 module IntellTerrec where
 
+{- Idee: Terminlisten der Tutorgruppen aus TUMOnline herunterladen
+ - und einlesen, beste Terminkombination nach folgenden Voraussetz-
+ - ungen finden:
+ - .. möglichst wenige Tage in der Uni
+ - .. möglichst kurze Wartezeiten
+ - .. möglichst kurze Laufwege -}
+ 
+ 
