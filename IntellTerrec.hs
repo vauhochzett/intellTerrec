29d7a8345dbc65c72b3e0769f87f3cc569a7a035
vauhochzett <valentin_zieglmeier@gmx.de>
2013-10-26 13:48:55 +0200

    Korrekte Implementierung der Typen

--- IntellTerrec.hs
+++ IntellTerrec.hs
@@ -10,20 +10,29 @@ module IntellTerrec where
 
 {- Typen -}
 
-data Time = Time { startTime :: Integer, endTime :: Integer} -- Von, Bis
-data Date = Date [(Integer, Time, Time)]
+type Hour = Integer
+type Minute = Integer
+
+data Time = Time { hour :: Hour, minute :: Minute}
+	deriving (Show)	-- Von, Bis
+
+
+
+
+{-data Date = Date {list :: [(Integer, Time, Time)]} 
+	[(Integer, Time, Time)] deriving (Show)-}
 
 
 {- Kontanten -}
 
 startTime1 :: Time
-startTime1 = Time (12,30)
+startTime1 = Time 12 30
 
 endTime1 :: Time
-endTime1 = Time (14,00)
+endTime1 = Time 14 00
 
-lv1 :: Date
-lv1 = Date [(0, startTime1, endTime1)]
+subtractTimes :: Time -> Time -> Bool
+subtractTimes time1 time2 = (hour time1) < (hour time2)
 
 
 {- IO -}
