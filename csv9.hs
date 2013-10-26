b91ccb35d76f3831d9d097d87ad678b887a82517
vauhochzett <valentin_zieglmeier@gmx.de>
2013-10-26 19:46:03 +0200

    Implementierung von "extractInfo"

--- csv9.hs
+++ csv9.hs
@@ -33,14 +33,32 @@ main =
             Left e -> do putStrLn "Error parsing input:"
                          print e
             --Right r -> mapM_ print r
+            Right r -> mapM_ print (extractInfo r)
 
 -- parseTest :: String -> 
 
 
 {- Extraktion der relevanten Informationen -}
 
-timeToDigits :: String -> [String]
-timeToDigits string = words [ switchDD a | a <- string ]
+extractInfo :: [[String]] -> GroupList
+extractInfo csvInput = [ Group group (extractDay day) (timeToDigits time1) (timeToDigits time2) | 
+  (day, a, time1, time2, b, c, d, e, group, f, g, h, i, j, k) <- csvInput ]
+
+--"DI","15.10.2013","10:15","11:45","90","220016771",
+--"Tutorübungen zu Analysis für Informatik [MA0902]",
+--"TT","Gruppe 17","MW 2235, Seminarraum (5502.02.235)",
+--"Abhaltung","feststehender Termin","",""
+
+extractDay :: String -> Day
+extractDay string 
+  | string == "MO" = 0
+  | string == "DI" = 1
+  | string == "MI" = 2
+  | string == "DO" = 3
+  | string == "FR" = 4
+
+timeToDigits :: String -> Time
+timeToDigits string = timeToDigits (words [ switchDD a | a <- string ])
 
 extractTime :: [String] -> Time
 extractTime timeString 
@@ -51,11 +69,3 @@ switchDD :: Char -> Char
 switchDD character 
 	| character == ':' = ' '
 	| otherwise = character
-
-extractDay :: String -> Day
-extractDay string 
-	| string == "MO" = 0
-	| string == "DI" = 1
-	| string == "MI" = 2
-	| string == "DO" = 3
-	| string == "FR" = 4
