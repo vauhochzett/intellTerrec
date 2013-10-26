61d4dfdf8ec7af80b2c0d4a6e2d1b39a274baf6b
vauhochzett <valentin_zieglmeier@gmx.de>
2013-10-26 17:10:48 +0200

    Implementierung der Match-Funktion

--- IntellTerrec.hs
+++ IntellTerrec.hs
@@ -29,18 +29,18 @@ data Group = Group {gNumber :: GNumber, gday :: Day, gstartTime :: Time, gendTim
 	deriving (Show)
 
 
-{- Konstanten -}
+{- Konstanten (Testing) -}
 
 freeSlot :: DateList
 freeSlot = 
 	[Date 0 (Time 11 30) (Time 13 00), 
 	Date 0 (Time 15 00) (Time 17 00),
-	Date 0 (Time 19 00) (Time 24 00)]
+	Date 0 (Time 19 00) (Time 23 00)]
 
 groupSlot :: GroupList
 groupSlot = 
-	[Group 1 0 (Time 10 00) (Time 12 00), 
-	Group 2 0 (Time 12 00) (Time 14 00), 
+	[Group 1 0 (Time 11 15) (Time 12 45), 
+	Group 2 0 (Time 12 00) (Time 13 30), 
 	Group 3 0 (Time 19 00) (Time 20 30)]
 
 
@@ -54,8 +54,8 @@ groupSlot =
 algorithm :: DateList -> GroupList -> GroupList
 algorithm freeSlots groupSlots
 	| length (match_1 freeSlots groupSlots) > 0 = match_1 freeSlots groupSlots
-	| length (match_2 freeSlots groupSlots) > 0 = match_2 freeSlots groupSlots
-	| length (match_3 freeSlots groupSlots) > 0 = match_3 freeSlots groupSlots
+	| length (match_2 15 freeSlots groupSlots) > 0 = match_2 15 freeSlots groupSlots
+	| length (match_2 30 freeSlots groupSlots) > 0 = match_2 30 freeSlots groupSlots
 	| otherwise = undefined
 
 
@@ -63,16 +63,12 @@ match_1 :: DateList -> GroupList -> GroupList
 match_1 freeSlots groupSlots = [ gs | gs <- groupSlots, fs <- freeSlots, 
 	(dday fs == gday gs) && (startWithin (dstartTime fs) (gstartTime gs)) && (endWithin (dendTime fs) (gendTime gs)) ]
 
-match_2 :: DateList -> GroupList -> GroupList
-match_2 freeSlots groupSlots = [ gs | gs <- groupSlots, fs <- freeSlots,
-	(dday fs == gday gs) && (startWithin15 (dstartTime fs) (gstartTime gs)) && (endWithin (dendTime fs) (gendTime gs)) ]
+match_2 :: Integer -> DateList -> GroupList -> GroupList -- todo
+match_2 amount freeSlots groupSlots = match_1 (expandSlots amount freeSlots) groupSlots
 
--- todo
-match_3 :: DateList -> GroupList -> GroupList
-match_3 freeSlots groupSlots = undefined
 
 startWithin :: Time -> Time -> Bool
-startWithin ftime gtime 
+startWithin ftime gtime
 	| hour ftime < hour gtime = True
 	| hour ftime == hour gtime && minute ftime <= minute gtime = True
 	| otherwise = False
@@ -83,6 +79,18 @@ endWithin ftime gtime
 	| hour ftime == hour gtime && minute ftime >= minute gtime = True
 	| otherwise = False
 
+
+-- Zeitslots erweitern
+
 expandSlots :: Integer -> DateList -> DateList -- Integer ist die Menge der Minuten (sollte nur 15 und 30 sein)
-expandSlots amount freeSlots
-	| = [Date (dday freeSlots) ((dstartTime freeSlots))
+expandSlots amount freeSlots = [ Date (dday fs) (expandStartTime amount (dstartTime fs)) (expandEndTime amount (dendTime fs)) | fs <- freeSlots ]
+
+expandStartTime :: Integer -> Time -> Time
+expandStartTime amount time
+	| minute time >= amount = Time (hour time) ((minute time)-amount)
+	| otherwise = Time ((hour time)-1) ((minute time)+(60-amount))
+
+expandEndTime :: Integer -> Time -> Time
+expandEndTime amount time
+	| minute time <= amount = Time (hour time) ((minute time)+amount)
+	| otherwise = Time ((hour time)+1) ((minute time)-(60-amount))
