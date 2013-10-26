263e726cf7816138d26073ab27e9431445d34f90
asaljic <strikerplayer@35.193.eduroam.dynamic.rbg.tum.de>
2013-10-26 14:01:42 +0200

    GroupSlots und Date - Typen hinzugefügt

--- IntellTerrec.hs
+++ IntellTerrec.hs
@@ -10,26 +10,29 @@ module IntellTerrec where
 
 {- Typen -}
 
+type Day = Integer
 type Hour = Integer
 type Minute = Integer
+type GNumber = Integer
 
 data Time = Time { hour :: Hour, minute :: Minute}
 	deriving (Show)	-- Von, Bis
 
 
+data Date = Date {datelist :: [(Day, Time, Time)]} 
+	deriving (Show)
 
-
-{-data Date = Date {list :: [(Integer, Time, Time)]} 
-	[(Integer, Time, Time)] deriving (Show)-}
+data GroupSlots = GroupSlots {groupList :: [(GNumber, Day, Time, Time)]}
+	deriving (Show)
 
 
 {- Kontanten -}
+freeSlot :: Date
+freeSlot = Date [(0, Time 11 30, Time 13 00), (0, Time 15 00, Time 17 00),
+	(0, Time 19 00, Time 24 00)]
 
-startTime1 :: Time
-startTime1 = Time 12 30
+groupSlot
 
-endTime1 :: Time
-endTime1 = Time 14 00
 
 subtractTimes :: Time -> Time -> Bool
 subtractTimes time1 time2 = (hour time1) < (hour time2)
